575e4b0a880524624d8fcc50a701d599cc87b475
unknown <vishalni2004@gmail.com>
2026-05-11 18:03:25 +0530

    chore: minor repository update

--- lib/google.ts
+++ lib/google.ts
@@ -2,6 +2,7 @@ import { google } from 'googleapis';
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
 const REDIRECT_URI = `${process.env.NEXT_PUBLIC_APP_URL}/api/integrations/google/callback`;
 
 export const createOAuth2Client = () => {
